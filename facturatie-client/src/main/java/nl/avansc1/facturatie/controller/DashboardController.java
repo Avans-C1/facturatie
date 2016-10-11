f1082841fe05be44f1b158d81ce18272f01c601e
Robin Valk <rjmvalk3@gmail.com>
2016-10-11 14:15:56 +0200

    Added basic crud: views, controller methods and repository. Added nav and improved layout a lot

--- facturatie-client/src/main/java/nl/avansc1/facturatie/controller/DashboardController.java
+++ facturatie-client/src/main/java/nl/avansc1/facturatie/controller/DashboardController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -19,6 +20,11 @@ class DashboardController {
 //    @Autowired
     public DashboardController() {
 
+    }
+
+    @ModelAttribute("page")
+    public String module() {
+        return "dashboard";
     }
 
     /**
