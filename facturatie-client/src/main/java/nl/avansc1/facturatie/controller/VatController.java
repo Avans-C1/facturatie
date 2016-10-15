e9a372eef194efc5465349b053bc670ec6411d29
Pascal van Hoof <pascal54321@hotmail.com>
2016-10-15 17:38:04 +0200

    Edit

--- facturatie-client/src/main/java/nl/avansc1/facturatie/controller/VatController.java
+++ facturatie-client/src/main/java/nl/avansc1/facturatie/controller/VatController.java
@@ -1,16 +1,15 @@
 package nl.avansc1.facturatie.controller;
 
 import nl.avansc1.facturatie.model.billing.Vat;
-import nl.avansc1.facturatie.model.customers.Customer;
-import nl.avansc1.facturatie.repository.CustomerDAO;
 import nl.avansc1.facturatie.repository.VatDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.util.Date;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by Pascal van Hoof on 13-10-2016.
@@ -26,7 +25,11 @@ public class VatController {
      * @return template/customer/overview.html
      */
     @RequestMapping("")
-    public String overview() {
+    public String overview(Model theModel) {
+
+        //Add invoices to model
+        theModel.addAttribute("vats", getVatList());
+
         return "vat/overview";
     }
 
@@ -36,17 +39,36 @@ public class VatController {
     }
 
 
+    @RequestMapping("/new/{id}")
+    public String edit(@PathVariable(value="id") int id, final ModelMap model){
+        System.out.println("Yeah lets edit :D " + id);
+        model.addAttribute("vat", VatDAO.findOne(id));
+
+        return "vat/add";
+    }
+
+
+
     @RequestMapping(value = "/new", method = RequestMethod.POST)
     public String addHandler(int percentage, final ModelMap model) {
         try {
             Vat vat = new  Vat(percentage);
             VatDAO.save(vat);
+
+            model.addAttribute("message", "Vat added to the database");
+            model.addAttribute("vats", getVatList());
+            return "vat/overview";
         } catch (Exception ex) { 
             return "vat/add";
         }
-        model.addAttribute("message", "Vat added to the database");
-        return "vat/overview";
+
     }
+
+    private Iterable<Vat> getVatList(){
+        return  VatDAO.findAll();
+    }
+
+
 
     @Autowired
     private VatDAO VatDAO;
