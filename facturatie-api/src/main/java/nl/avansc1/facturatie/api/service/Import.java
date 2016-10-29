825fa5a5a968cf34e1237021f00d1c1015b76205
Bob van der Valk <info@mawoo.nl>
2016-10-29 14:38:59 +0200

    Import xml files and save them with hibernate

--- facturatie-api/src/main/java/nl/avansc1/facturatie/api/service/Import.java
+++ facturatie-api/src/main/java/nl/avansc1/facturatie/api/service/Import.java
@@ -1,9 +1,16 @@
 package nl.avansc1.facturatie.api.service;
 
+import nl.avansc1.facturatie.api.model.billing.Declaration;
+import nl.avansc1.facturatie.api.model.billing.Treatment;
+import nl.avansc1.facturatie.api.model.customers.Customer;
+import nl.avansc1.facturatie.api.repository.CustomerDao;
 import nl.avansc1.facturatie.api.repository.DeclarationDAO;
 import nl.avansc1.facturatie.api.repository.TreatmentDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -11,6 +18,7 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
+import java.util.Date;
 
 /**
  * This class is responsible to import new declarations
@@ -31,10 +39,33 @@ public class Import {
         DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
         return new Import(dBuilder.parse(file));
     }
+    //TODO: finish this function when the other group api is done
+    public static Import importWithURL(String url) {
+        return null;
+    }
 
     public Import(Document document) {
         this.document = document;
     }
+
+    public void start() {
+        NodeList nList = document.getElementsByTagName("treatments");
+
+        for (int temp = 0; temp < nList.getLength(); temp++) {
+            Node nNode = nList.item(temp);
+            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                Element eElement = (Element) nNode;
+                Customer customer = customerDao.findByCsn(Integer.parseInt(eElement.getAttribute("customer")));
+                Treatment treatment = treatmentDAO.findOne(Integer.parseInt(eElement.getAttribute("code")));
+                Date today = new Date();
+                Declaration declaration = new Declaration(customer, treatment, today, treatment.getPrice());
+                declarationDAO.save(declaration);
+            }
+        }
+    }
+
+    @Autowired
+    private CustomerDao customerDao;
 
     @Autowired
     private DeclarationDAO declarationDAO;
