3e14a47492c97e6cd0717aa8945de3280abee30f
Bob van der Valk <info@mawoo.nl>
2016-10-29 14:41:23 +0200

    Add working controller. XML IMPORTER DONE not tested yet

--- facturatie-api/src/main/java/nl/avansc1/facturatie/api/controller/ImportController.java
+++ facturatie-api/src/main/java/nl/avansc1/facturatie/api/controller/ImportController.java
@@ -1,7 +1,12 @@
 package nl.avansc1.facturatie.api.controller;
 
+import nl.avansc1.facturatie.api.service.Import;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
 
 /**
  * This controller is responsible for importing declarations
@@ -11,5 +16,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/import")
 @Controller
 public class ImportController {
-
+    @RequestMapping("/")
+    public String importRoute() throws ParserConfigurationException, SAXException, IOException {
+        Import xml = Import.importWithTestFile("treatments.xml");
+        xml.start();
+        return "Import succesfully completed";
+    }
 }
